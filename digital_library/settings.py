978f094a9c38421536a69d7a24855840caf461ce
aushamim <au.shamim0@gmail.com>
2024-04-07 22:49:09 +0600

    Add URL to allowed hosts

--- digital_library/settings.py
+++ digital_library/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-digital-library.onrender.com"]
 
 
 # Application definition
